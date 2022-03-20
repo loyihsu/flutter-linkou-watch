2af3e93d4f093488b619ef9251bebbb10c36c30e
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-03-20 09:57:42 +0800

    Remove demo codes for documentation.

--- lib/ContentWidget.dart
+++ lib/ContentWidget.dart
@@ -6,70 +6,37 @@ class ContentWidget extends StatefulWidget {
   State<StatefulWidget> createState() => ContentWidgetState();
 }
 
-// class ContentWidgetState extends State<ContentWidget> {
-//   final String _title = "林口 - 圓山";
-//   String _speed = "均速 100 公里";
-//   String _estimation = "約 2 分鐘";
-
-//   void setSpeed(int newSpeed) {
-//     setState(() {
-//       _speed = "均速 " + newSpeed.toString() + " 公里";
-//     });
-//   }
-
-//   void setEstination(int newEstimation) {
-//     setState(() {
-//       _estimation = "約" + newEstimation.toString() + "分鐘";
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       decoration: const BoxDecoration(
-//           shape: BoxShape.rectangle,
-//           color: Colors.cyan,
-//           borderRadius: BorderRadius.all(Radius.circular(20))),
-//       child: Padding(
-//           padding: const EdgeInsets.all(15),
-//           child: Column(children: <Widget>[
-//             Text(_title, style: Theme.of(context).textTheme.headline5),
-//             Text(_speed),
-//             Text(_estimation),
-//           ])),
-//     );
-//   }
-// }
-
 class ContentWidgetState extends State<ContentWidget> {
-  final String _title = "Linkou to Yuanshan";
-  String _speed = "100 kmps";
-  String _estimation = "2 minutes";
+  final String _title = "林口 - 圓山";
+  String _speed = "均速 100 公里";
+  String _estimation = "約 2 分鐘";
 
   void setSpeed(int newSpeed) {
     setState(() {
-      _speed = newSpeed.toString() + " kmps";
+      _speed = "均速 " + newSpeed.toString() + " 公里";
     });
   }
 
   void setEstination(int newEstimation) {
     setState(() {
-      _estimation = newEstimation.toString() + " minutes";
+      _estimation = "約" + newEstimation.toString() + "分鐘";
     });
   }
 
+  @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: const BoxDecoration(
-            shape: BoxShape.rectangle,
-            color: Colors.cyan,
-            borderRadius: BorderRadius.all(Radius.circular(20))),
-        child: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Column(children: <Widget>[
-              Text(_title, style: Theme.of(context).textTheme.headline5),
-              Text(_speed),
-              Text(_estimation),
-            ])));
+      decoration: const BoxDecoration(
+          shape: BoxShape.rectangle,
+          color: Colors.cyan,
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+      child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(children: <Widget>[
+            Text(_title, style: Theme.of(context).textTheme.headline5),
+            Text(_speed),
+            Text(_estimation),
+          ])),
+    );
   }
 }
